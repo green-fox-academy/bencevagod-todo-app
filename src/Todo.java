763ee1205450fc4139f3004d64c02fa2f9420e94
Bencevagod <bajkaibence@gmail.com>
2020-06-10 16:12:16 +0200

    works flawlessly

--- src/Todo.java
+++ src/Todo.java
@@ -8,16 +8,13 @@ import java.util.List;
 public class Todo {
     public static void main(String[] args) {
         List<String> todos = new ArrayList<>();
-        Path path = Paths.get("/Users/bence/documents/github/bencevagod-todo-app/assets/todolist.txt");
+        Path path = Paths.get("../assets/todolist.txt");
 
         if (args.length == 0) {
-            System.out.println();
-            System.out.println("Command Line Todo application");
-            System.out.println("=============================");
-            System.out.println();
-            System.out.println("Command line arguments: \n    -l    Lists all the tasks\n    -a    Adds a new task\n" +
-                "    -r    Removes a task\n    -c    Completes a task");
-            System.out.println();
+            printUsage();
+        } else if(!(args[0].equals("-l") || args[0].equals("-a") || args[0].equals("-r") || args[0].equals("-c"))) {
+            System.out.println("Unsupported argument");
+            printUsage();
         } else if (args[0].equals("-l")) {
             try {
                 todos = Files.readAllLines(path);
@@ -42,7 +39,7 @@ public class Todo {
             } else {
                 try {
                     todos = Files.readAllLines(path);
-                    todos.add(args[1]);
+                    todos.add("[ ] "+args[1]);
                     Files.write(path, todos);
                 } catch (IOException e) {
                     System.out.println("Cannot write file");
@@ -58,7 +55,49 @@ public class Todo {
             } catch (IOException e) {
                 System.out.println("Cannot delete from file");
                 System.exit(2);
+            } catch (IndexOutOfBoundsException e) {
+                if (args.length < 2) {
+                    System.out.println("Unable to remove: index is not provided");
+                    System.exit(2);
+                } else {
+                System.out.println("Unable to remove: index is out of bound");
+                System.exit(2);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Unable to remove: index is not a number");
+            }
+        } else if (args[0].equals("-c")) {
+            try {
+                todos = Files.readAllLines(path);
+                int indexToOverWrite = Integer.parseInt(args[1]) - 1;
+//                char[] placingX = todos.get(indexToOverWrite).toCharArray();
+//                placingX[1] = 'x';
+//                String completedTask = String.valueOf(placingX);
+                todos.set(indexToOverWrite, "[x"+todos.get(indexToOverWrite).substring(2));
+                Files.write(path, todos);
+            } catch (IOException e) {
+                System.out.println("Cannot write to file");
+                System.exit(2);
+            } catch (IndexOutOfBoundsException e) {
+                if (args.length < 2) {
+                    System.out.println("Unable to check: index is not provided");
+                    System.exit(2);
+                } else {
+                    System.out.println("Unable to check: index is out of bound");
+                    System.exit(2);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Unable to check: index is not a number");
             }
         }
+    }
+    public static void printUsage() {
+        System.out.println();
+        System.out.println("Command Line Todo application");
+        System.out.println("=============================");
+        System.out.println();
+        System.out.println("Command line arguments: \n    -l    Lists all the tasks\n    -a    Adds a new task\n" +
+                "    -r    Removes a task\n    -c    Completes a task");
+        System.out.println();
     }
 }
